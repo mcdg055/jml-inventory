d657c5984f22e3c92b2e0d202ceacc213583a70b
martin055 <deguzmanmartinc@gmail.com>
2022-12-23 10:20:17 +0800

    System Module
    initial commit

---
+++ modules/system/app/Controllers/SystemController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\System\App\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class SystemController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+}
